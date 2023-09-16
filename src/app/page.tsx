ad9945e8772e819038b3856e63747e2a27b2534c
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-16 18:49:20 +0300

    feat: cardcontainer to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+
+import CardContainer from '@/cardContainer/CardContainer'
 import styles from './page.module.scss'
 import _switch from '@/store/switch'
 import { observer } from 'mobx-react-lite'
@@ -7,6 +9,7 @@ import React from 'react';
 const Home = observer(() => {
   return (
     <main className={styles.main}>
+      {_switch.button === _switch.chose[1] && <CardContainer/>}
     </main>
   )
 }
