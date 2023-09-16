d2269a633b8b21d726016735635b548871da4a32
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-16 18:48:48 +0300

    feat: switch state to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 "use client"
 import styles from './page.module.scss'
+import _switch from '@/store/switch'
+import { observer } from 'mobx-react-lite'
 import React from 'react';
+
+const Home = observer(() => {
   return (
     <main className={styles.main}>
     </main>
   )
 }
+)
+export default Home
