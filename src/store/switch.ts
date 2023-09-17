95ee240afec76de0338e6b002cf40a2d695698e5
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-17 21:06:14 +0300

    feat: date calculations and card adding functions

--- src/store/switch.ts
+++ src/store/switch.ts
@@ -1,12 +1,13 @@
 import { makeAutoObservable } from "mobx"
-import data from './../data.json'
+import dataArr from './../data.json'
 class Switch {
 
   chose = ['Таблица', 'Карточки']
   button = this.chose[0]
-  data = data
+  data:any = []
   URL ='past API here'
-  current = 0
+  current = -1
+  index = 0
   
   constructor(){
     makeAutoObservable(this)
@@ -17,27 +18,42 @@ class Switch {
     console.log(this.button)
   }
 
-  // TODO: для прода замнить data в 9 строке на пустой массив
-  fetchData(){
-    fetch(this.URL )
-      .then(response => response.json())
-      .then(json => {
-        this.data = [...json]
-      })
-  }
-
   mark(id: number){
     this.current = id
   }
 
   checkRead(event: any){
     event.preventDefault()
-    if(event.code === 'Space') this.data.forEach(i => {
+    if(event.code === 'Space') this.data.forEach((i: { id: number; isRead: boolean }) => {
       if(i.id === this.current){
         i.isRead = true
       } else {
       }
     })
+  }
+
+  setDate() {
+    const start = new Date(2023, 0, 1);
+    const end = new Date();
+    const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  
+    const options = {
+                      year: 'numeric',
+                      month: '2-digit',
+                      day: '2-digit',
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      second: '2-digit',
+                      spacer: 'none'
+                    };
+    return date.toLocaleString('ru-RU').replace(',', '')
+  }
+
+  featData(){
+    this.data.push(dataArr[this.index % 6])
+    this.data[this.index].date = this.setDate()
+    this.data[this.index].id = this.index
+    this.index++
   }
 }
 
