99eb410503313cd172d8aec84e9e1a47109c97d7
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-17 21:11:10 +0300

    refactor: feat new mark every second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,16 @@ import CardContainer from '@/cardContainer/CardContainer'
 import styles from './page.module.scss'
 import _switch from '@/store/switch'
 import { observer } from 'mobx-react-lite'
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Home = observer(() => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('This will run every second!');
+      _switch.featData()
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <main className={styles.main}>
       {_switch.button === _switch.chose[1] && <CardContainer/>}
