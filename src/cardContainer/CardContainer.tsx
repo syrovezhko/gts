c443cd80c61745165f387d4480800da361aa8750
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-17 20:27:06 +0300

    feat: pagination to cardContainer

--- src/cardContainer/CardContainer.tsx
+++ src/cardContainer/CardContainer.tsx
@@ -4,17 +4,18 @@ import Card from '@/card/Card';
 import _switch from '@/store/switch';
 import { observer } from 'mobx-react-lite';
 import useDate from '@/hooks/useDate';
+import paginator from '@/store/paginator';
 
 const CardContainer = observer(() => {
   return (
     <div
       className={styles.container}
     >
-      {_switch.data.map((_, i) => 
+      {_switch.data.slice(paginator.first, paginator.first + paginator.rows).map((_, i) => 
         <Card
-          key={i}
+          key={i+paginator.first}
           date={useDate()}
-          i={i}
+          i={i+paginator.first}
         />)}
     </div>
   );
