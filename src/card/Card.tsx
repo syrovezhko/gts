13d7469493906cc49237fec2b653d5f24d3bf7e0
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-17 19:22:19 +0300

    feat: card content

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -1,35 +1,62 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './Card.module.scss'
-
+import Image from 'next/image';
+import _switch from '@/store/switch';
+import { observer } from 'mobx-react-lite';
 interface IData {
-  key: number;
-  user: number;
-  name: string;
-  equipment: string | null;
-  message: string;
-  importance: string;
   date: string;
+  i: number;
 }
 
-const Card: FC<IData> = ({
-              key,
-              user,
-              name,
-              equipment,
-              message,
-              importance,
-              date
-            }) => {
+const Card: FC<IData> = observer(({ date, i }) => {
+  const [card, setCard] = useState(`${styles.card}`)
+  useEffect(()=>{
+    if(_switch.data[i].id === _switch.current) {
+      setCard(`${styles.card} ${styles.card_active}`)
+    } else {
+      setCard(`${styles.card}`)
+    }
+  },[_switch.current])
+
   return (
-    <div className={styles.card}>
-      <p>{date}</p>
-      <p>{user}</p>
-      <p>{name}</p>
-      <p>{equipment}</p>
-      <p>{message}</p>
-      <p>{importance}</p>
+    <div
+      className={card}
+      onClick={()=>_switch.mark(_switch.data[i].id)}
+      onKeyDown={e=>_switch.checkRead(e)}
+      tabIndex={-1}
+    >
+      <div className={styles.card__container}>
+        <ul className={styles.list}>
+          <li>Дата</li>
+          <li>Важность</li>
+          <li>Оборудование</li>
+          <li>Сообщение</li>
+        </ul>
+        <ul className={styles.list}>
+          <li>{date}</li>
+          <li>
+            {_switch.data[i].isRead ?
+            <span className={`${styles.isRead} ${styles.isRead_done}`}>X</span>:
+            <span className={styles.isRead}></span>
+            }
+            {_switch.data[i].importance}
+            </li>
+          <li>{_switch.data[i].equipment}</li>
+          <li>{_switch.data[i].message}</li>
+        </ul>
+        <div className={styles.user}>
+          <Image
+            src='/user.jpg'
+            width={70}
+            height={70}
+            alt={`Фотография пользователя ${_switch.data[i].userName}`}
+            className={styles.user__img}
+          />
+          <p>{_switch.data[i].userName}</p>
+        </div>
+      </div>
     </div>
   );
-};
+});
 
 export default Card;
